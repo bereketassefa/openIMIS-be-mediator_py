9ede8989f80040d69cf12810c2d9c027a873adb9
bereketassefa <bereket.a.gedefaw@gmail.com>
2025-04-14 16:46:46 +0300

    bug fix

--- mediator/test.py
+++ mediator/test.py
@@ -1,12 +1,13 @@
 import requests
 from django.conf import settings
 import json
-from .models import EMRServer
+# from .models import EMRServer
 from gql.transport.requests import RequestsHTTPTransport
 import re
 
 def fetch_data_from_server():
-    EMR_list = EMRServer.objects.all()
+    # EMR_list = EMRServer.objects.all()
+    EMR_list = ['http://localhost:4000/claims']
     results = []
     
     session = requests.Session()
@@ -14,7 +15,7 @@ def fetch_data_from_server():
     try:
         for server in EMR_list:
             try:
-                response = session.get(server.api_endpoint, timeout=10)
+                response = session.get(server, timeout=10)
                 
                 response.raise_for_status()
                 
@@ -22,9 +23,9 @@ def fetch_data_from_server():
                 
                 results.append(data)
                 
-                if data:
-                    server.last_claim_id = data[-1]['id'] 
-                    server.save()
+                # if data:
+                #     server.last_claim_id = data[-1]['id'] 
+                #     server.save()
                 
             except requests.exceptions.RequestException as e:
                 print(f"Error fetching {server}: {str(e)}")
@@ -34,7 +35,8 @@ def fetch_data_from_server():
     finally:
         session.close()
     
-    save_claims_to_db(results)
+    save_claims_to_db(results[0])
+    
     
     return results
 
@@ -45,69 +47,66 @@ def save_claims_to_db(claims):
     
     try:
         credentialresponse = get_jwt_token(url)
-        print(f"Credential response: {credentialresponse.len()}")
-        jwt , session = credentialresponse.values()
+        jwt , session = credentialresponse
         csrf_token = get_csrf_token(url ,jwt , session)
-        for index , claim in claims:
+        for index , claim in enumerate(claims):
             try:
-                print(f"Saving claim {index} to database")
-                print(f"Claim data: {claim}")
-                mutation_variables = {
-                    "input": {
-                        "clientMutationLabel": f"Create Claim - {claim['code']}",
-                        "code": claim["code"],
-                        "insureeId": claim["insureeId"],
-                        "dateFrom": claim["dateFrom"],
-                        "icdId": claim["icdId"],
-                        "dateClaimed": claim["dateClaimed"],
-                        "healthFacilityId": claim["healthFacilityId"],
-                        "services": [
-                            {
-                                "serviceId": service["serviceId"],
-                                "priceAsked": service["priceAsked"],
-                                "qtyProvided": service["qtyProvided"],
-                                "serviceServiceSet": service.get("serviceServiceSet", []),
-                                "serviceItemSet": service.get("serviceItemSet", []),
-                                "status": service["status"]
-                            }
-                            for service in claim["services"]
-                        ]
-                    }
-                }
                 query = """
-                   type Mutation {
+                    mutation  {
                         createClaim(input: {
-                            clientMutationLabel: String
-                            code: String!
-                            insureeId: Int!
-                            dateFrom: String!
-                            icdId: Int!
-                            dateClaimed: String!
-                            healthFacilityId: Int!
-                            services: [{
-                                serviceId: Int!
-                                priceAsked: String!
-                                qtyProvided: String!
-                                status: Int!
-                            }]!
-                        }){
-                            clientMutationId: String
-                            internalId: String
+                            clientMutationLabel: "%s",
+                            code: "%s",
+                            insureeId: %d,
+                            dateFrom: "%s",
+                            icdId: %d,
+                            dateClaimed: "%s",
+                            healthFacilityId: %d,
+                            services: [
+                                %s
+                            ]
+                        }) {
+                            clientMutationId
+                            internalId
                         }
-                    }"""
+                    }
+                    """ % (
+                        claim['code'],
+                        claim['code'],
+                        claim['insureeId'],
+                        claim['dateFrom'],
+                        claim['icdId'],
+                        claim['dateClaimed'],
+                        claim['healthFacilityId'],
+                        ', '.join([
+                            """
+                            {
+                                serviceId: %d,
+                                priceAsked: %s,
+                                qtyProvided: %s,
+                                status: "%d"
+                            }
+                            """ % (
+                                service['serviceId'],
+                                service['priceAsked'],
+                                service['qtyProvided'],
+                                service['status']
+                            )
+                            for service in claim['services']
+                        ])
+                    )
                 headers = {
                     "Cookie": f"JWT={jwt}; openimis_session={session}",
                     'Content-Type': 'application/json',
                     'x-csrftoken': csrf_token
                 }
+                print(query)
+                response = requests.post(url, headers=headers, json={'query': query})
                 print("Mutation response:")
-                response = requests.post(url, headers=headers, json={'query': query, 'variables': mutation_variables})
-                response.raise_for_status()
-                result = response.json()
-                print("Mutation response:")
-                print(result)
+                print(response.json())
+                if response.status_code != 200:
+                    raise Exception(f"Error saving claim : {response.text}")
             except Exception as e:
-                print(f"Error saving claim {claim}: {str(e)}")
+                print(f"Error saving claim : {str(e)}")
     except Exception as e:
         print(f"Error receiving JWT token: {e}")
 
@@ -134,11 +133,8 @@ def get_jwt_token(url):
         jwt_match = re.search(r'JWT=([^;]+)', set_cookie_value)
         session_match = re.search(r'openimis_session=([^;]+)', set_cookie_value)
         if jwt_match and session_match:
-            jwt_token = jwt_match.group(1)
-            session_token = session_match.group(1)
-            print(f"jwt_token: {jwt_token}")
-            print(f"session_token: {session_token}")
-            return (jwt_token , session_token)
+            return (jwt_match.group(1), session_match.group(1))
+            return ("" , "")
         else:
             raise ValueError("No JWT token or session found in set-cookie header")
             
@@ -171,3 +167,11 @@ def get_csrf_token(url ,jwt , session):
         raise Exception(f"HTTP error occurred: {str(e)}") from e
     except json.JSONDecodeError as e:
         raise Exception("Response content is not valid JSON") from e
+
+fetch_data_from_server()
+
+
+# tried removing csrf filter
+# figured a way to get the csrf token
+# updating the required fields requested from EMR  servers
+#
